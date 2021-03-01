9a9cd56108154f7e81fb5df8ee2ba18cb3c13ddf
arcqk5 <grigoriylafutkin@yandex.ru>
2021-03-02 00:03:50 +0300

    added new methods on array

---
+++ JS--Methods/12/EveryAndSome.js
@@ -0,0 +1,25 @@
+//Every and Some
+//js--methods
+
+//every
+const arr = [4, 5, 6];
+let b = arr.every(item => {
+    if (item > 3) return true;
+});
+
+console.log(b); //true
+
+let c = arr.every(item => {
+    if (item > 4) return true;
+});
+
+console.log(c); //false
+
+
+//some (если хотя бы один эл-т подошел под условие => true)
+const ar2r = [4, 5, 6];
+let b2 = arr2.some(item => {
+    if (item > 3) return true;
+});
+
+console.log(b2); //true
