8d0c50db5f1f33ce04d24dd9fa918e63df5484db
arcqk5 <grigoriylafutkin@yandex.ru>
2021-03-01 23:16:19 +0300

    added isArray methods on array

---
+++ JS--Methods/8/isArray.js
@@ -0,0 +1,10 @@
+//isArray
+//js--methods
+
+const array = [12345678, 789, 35423567, 722];
+const num = 45;
+const set = new Set([1, 2, 3]);
+
+console.log(Array.isArray(array)); // true
+console.log(Array.isArray(num)); // false
+console.log(Array.isArray(set)); // false
