6ab027a929f3f70005efb9062fdd4444d3b01c9f
arcqk5 <grigoriylafutkin@yandex.ru>
2021-03-01 23:37:25 +0300

    added new methods on array

---
+++ JS--Methods/10/SpliceAndConcat.js
@@ -0,0 +1,14 @@
+// Splice and Concat
+// js--methods
+
+//splice
+let array = [11, 12, 14, 19];
+
+
+
+//concat
+const arr_1 = [11, 12, 14, 19];
+const arr_2 = [18, 12, 15, 19];
+
+const conc = arr_1.concat(arr_2);
+console.log(conc); //[11, 12, 14, 19, 18, 12, 15, 19]
