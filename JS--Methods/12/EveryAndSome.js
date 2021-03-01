7d437a902f018cac4b1925eb285bdce0781d77ab
arcqk5 <grigoriylafutkin@yandex.ru>
2021-03-02 00:04:58 +0300

    ch - some methods

--- JS--Methods/12/EveryAndSome.js
+++ JS--Methods/12/EveryAndSome.js
@@ -17,7 +17,7 @@ console.log(c); //false
 
 
 //some (если хотя бы один эл-т подошел под условие => true)
-const ar2r = [4, 5, 6];
+const arr2 = [4, 5, 6];
 let b2 = arr2.some(item => {
     if (item > 3) return true;
 });
